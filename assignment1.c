359ffa84fef716887bc070c69aab9b3897594d30
white184 <47287853+white184@users.noreply.github.com>
2019-02-12 02:12:29 -0800

    Add files via upload

---
+++ assignment1.c
@@ -0,0 +1,34 @@
+/*
+Chawon White - 000833921
+CS433 - Operating Systems
+01.25.2019 - 02.12.2019
+Project 1 - Introduction to Linux Kernel Modules
+*/
+ 
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/jiffies.h>
+#include <linux/hash.h>
+#include <linux/gcd.h>
+#include <asm/param.h>
+ 
+/* This function is called when the module is loaded. */
+int simple_init(void)
+{
+printk(KERN_INFO "Loading Kernel Module\n");
+printk(KERN_INFO "%lu\n", GOLDEN_RATIO_PRIME);
+printk(KERN_INFO "%lu\n", jiffies);
+printk(KERN_INFO "%lu\n", HZ);
+return 0;
+}
+/* This function is called when the module is removed. */
+void simple_exit(void)
+{
+printk(KERN_INFO "Removing Kernel Module\n");
+printk(KERN_INFO "%lu\n", gcd(3300,24));
+printk(KERN_INFO "%lu\n", jiffies);
+}
+/* Macros for registering module entry and exit points. */
+module_init(simple_init);
+module_exit(simple_exit);
